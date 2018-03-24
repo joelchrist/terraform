1104cdbe4f8861ee11c1be3ec246dd172695cf10
Joel Christ <joel@joelchrist.nl>
2018-03-24 12:22:42 +0100

    Add default ACL to shorty bucket

--- modules/buckets/main.tf
+++ modules/buckets/main.tf
@@ -8,3 +8,8 @@ resource "google_storage_bucket" "shorty_bucket" {
   name = "shorty-url-shortener"
   location = "europe-west1"
 }
+
+resource "google_storage_bucket_acl" "shorty_acl" {
+  bucket = "${google_storage_bucket.shorty_bucket.name}"
+  predefined_acl = "publicRead"
+}
